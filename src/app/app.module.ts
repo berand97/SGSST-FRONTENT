781a87825cf6aa39d524400b4b4517c987387c91
Alejandra Jaramillo <yitneda.jaramillov@unac.edu.co>
2021-03-21 18:14:41 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+const routes: Routes= [
+  { path: 'login', component: LoginComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
